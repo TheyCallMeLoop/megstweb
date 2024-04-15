2d8e876601bf903fc53d325b10af856295e0f167
TheyCallMeLoop <143710422+TheyCallMeLoop@users.noreply.github.com>
2024-04-15 17:41:22 +0500

    abuzar

--- src/screens/onbording/index.jsx
+++ src/screens/onbording/index.jsx
@@ -1,20 +1,19 @@
-import React, { useRef, useState } from 'react'
-import BgVideo from '../../assets/video/videomgst.mp4';
-import BaseButton from '../../compnents/basebutton';
-import logo from '../../assets/images/Logo.svg'
-import { Button, Modal } from 'react-bootstrap';
+import React, { useRef, useState } from "react";
+import BgVideo from "../../assets/video/videomgst.mp4";
+import BaseButton from "../../compnents/basebutton";
+import logo from "../../assets/images/Logo.svg";
+import { Button, Modal } from "react-bootstrap";
 import * as Yup from "yup";
-import { useFormik } from 'formik';
+import { useFormik } from "formik";
 // import { useHistory } from 'react-router-dom';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
-import upload from '../../assets/images/cloud-computing.png'
-import {ArrowLeft} from 'react-feather'
+import upload from "../../assets/images/cloud-computing.png";
+import { ArrowLeft } from "react-feather";
 export default function Onboarding() {
-
   const navigation = useNavigate();
 
-  const [imageUrl, setImageUrl] = useState('');
+  const [imageUrl, setImageUrl] = useState("");
 
   const [showModal, setShowModal] = useState(false);
   const [showModal2, setShowModal2] = useState(false);
@@ -22,27 +21,26 @@ export default function Onboarding() {
   const [companymodal, setCompanymodal] = useState(false);
   const [questionmodal, setQuestionmodal] = useState(false);
 
-
   const fileInputRef = useRef(null);
 
   // const history = useHistory();
 
   const formik = useFormik({
+    validateOnMount: true, // <================= Add this
     initialValues: {
       name: "",
       middlename: "",
       nationality: "",
       company: "",
     },
-    onSubmit: values => {
-      setShowModal2(false)
-      setCompanymodal(true)
+    onSubmit: (values) => {
+      console.log(values);
+      setShowModal2(false);
+      setCompanymodal(true);
       // setShowModal3(true)
       // history.push('/dashboard')
-
     },
   });
-
 
   const formik2 = useFormik({
     initialValues: {
@@ -53,19 +51,18 @@ export default function Onboarding() {
       facebook: "",
       located: "",
     },
-    onSubmit: values => {
-      setCompanymodal(false)
-      setQuestionmodal(true)
+    onSubmit: (values) => {
+      setCompanymodal(false);
+      setQuestionmodal(true);
     },
   });
 
   const formik3 = useFormik({
     initialValues: {
-      why: ""
+      why: "",
     },
-    onSubmit: values => { 
-      navigation('/dashboard')
-
+    onSubmit: (values) => {
+      navigation("/dashboard");
     },
   });
   const handleVideoEnd = () => {
@@ -73,16 +70,13 @@ export default function Onboarding() {
   };
   const Sginupmodal = () => {
     setShowModal(false);
-    setShowModal2(true)
-  }
-
+    setShowModal2(true);
+  };
 
   // const removeImage = () => {
   //   setSelectedImg([]);
   //   setImages([]);
   // };
-
-
 
   const handleImageChange = (event) => {
     const file = event.target.files[0];
@@ -95,13 +89,9 @@ export default function Onboarding() {
     }
   };
 
-
   const handleImageClick = () => {
     fileInputRef.current.click();
   };
-
-
-
 
   const handleSubmit = (values) => {
     console.log(values);
@@ -111,192 +101,270 @@ export default function Onboarding() {
       <video autoPlay muted className="bg-vid" onEnded={handleVideoEnd}>
         <source src={BgVideo} type="video/mp4" />
       </video>
-      <div className='container_onboard'>
-        <div className='flex_space'>
-          <Button className='base_btn font_regular text_white' onClick={() => Sginupmodal()}  >Apply Now</Button>
-          <img src={logo} alt="" />
+      <div className="container_onboard">
+        <div className="flex_space">
+          <Button
+            className="base_btn font_regular text_white"
+            onClick={() => Sginupmodal()}
+          >
+            Apply Now
+          </Button>
+          <img style={{ width: 100 }} src={logo} alt="" />
         </div>
       </div>
 
       <Modal
         aria-labelledby="contained-modal-title-vcenter"
         centered
-        show={showModal} onHide={() => setShowModal(false)}>
-        <Modal.Body className='onboard_modal'>
-          <p className='font_regular text_white fs_20 text-center'>To continue exploring, we invite you to apply for our access list.</p>
-          <Button className='m_0 text_white font_regular modal_btn ' onClick={() => Sginupmodal()} >Click here</Button>
+        show={showModal}
+        onHide={() => setShowModal(false)}
+      >
+        <Modal.Body className="onboard_modal">
+          <p className="font_regular text_white fs_20 text-center">
+            To continue exploring, we invite you to apply for our access list.
+          </p>
+          <Button
+            className="m_0 text_white font_regular modal_btn "
+            onClick={() => Sginupmodal()}
+          >
+            Click here
+          </Button>
         </Modal.Body>
       </Modal>
 
-
-      <Modal className='abc' show={showModal2} onHide={() => setShowModal2(false)} aria-labelledby="contained-modal-title-vcenter"
-        centered>
-        <Modal.Body className='bg_primary radius_10'>
-          <div className='onboard_modal_sginup '>
-          <h1 className='text-center text_white font_medium fs_30'>Personal Details</h1>
-          <p className='text-center text_white font_regular'>Fill up the below details</p>
+      <Modal
+        className="abc"
+        show={showModal2}
+        onHide={() => setShowModal2(false)}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Body className="bg_primary radius_10">
+          <div className="onboard_modal_sginup ">
+            <h1 className="text-center text_white font_medium fs_30">
+              Personal Details
+            </h1>
+            <p className="text-center text_white font_regular">
+              Fill up the below details
+            </p>
             <form onSubmit={formik.handleSubmit}>
-              <p className='font_bold text_white mb-2'>Name</p>
-              <input id="name"
+              <p className="font_bold text_white mb-2">Name</p>
+              <input
+                id="name"
                 name="name"
                 type="text"
-                placeholder='Enter your name '
+                placeholder="Enter your name "
                 onChange={formik.handleChange}
                 value={formik.values.name}
-                className='input_felid' />
-              <p className='font_bold text_white mb-2 mt-2'>Middle Name</p>
-              <input className='input_felid'
+                className="input_felid"
+              />
+              <p className="font_bold text_white mb-2 mt-2">Middle Name</p>
+              <input
+                className="input_felid"
                 id="middlename"
                 name="middlename"
-                placeholder='Enter your middle name'
+                placeholder="Enter your middle name"
                 type="text"
                 onChange={formik.handleChange}
-                value={formik.values.middlename} />
+                value={formik.values.middlename}
+              />
 
-              <p className='font_bold text_white mb-2 mt-2'>Nationality </p>
-              <input id="nationality"
+              <p className="font_bold text_white mb-2 mt-2">Nationality </p>
+              <input
+                id="nationality"
                 name="nationality"
-                placeholder='Enter your nationality'
+                placeholder="Enter your nationality"
                 type="text"
                 onChange={formik.handleChange}
-                value={formik.values.nationality} className='input_felid' />
+                value={formik.values.nationality}
+                className="input_felid"
+              />
 
-
-              <p className='font_bold text_white mb-2 mt-2'>Company</p>
-              <input id="company"
+              <p className="font_bold text_white mb-2 mt-2">Company</p>
+              <input
+                id="company"
                 name="company"
                 type="text"
-                placeholder='Enter your company name'
+                placeholder="Enter your company name"
                 onChange={formik.handleChange}
-                value={formik.values.company} className='input_felid' />
+                value={formik.values.company}
+                className="input_felid"
+              />
 
-              <p className='font_bold text_white mb-2 mt-2'>Passport</p>
+              <p className="font_bold text_white mb-2 mt-2">Passport</p>
 
-              <div className='upload'>
-                <input type="file" style={{ display: 'none' }}
-                  ref={fileInputRef} onChange={handleImageChange}
-                  className='upload_feild' accept="image/*" />
-                <img onClick={handleImageClick}
-                  src={upload} width={30} height={30} style={{ cursor: 'pointer' }} alt="" />
+              <div className="upload">
+                <input
+                  type="file"
+                  style={{ display: "none" }}
+                  ref={fileInputRef}
+                  onChange={handleImageChange}
+                  className="upload_feild"
+                  accept="image/*"
+                />
+                <img
+                  onClick={handleImageClick}
+                  src={upload}
+                  width={30}
+                  height={30}
+                  style={{ cursor: "pointer" }}
+                  alt=""
+                />
               </div>
-              <Button disabled={!formik.isValid} className='m_0 text_white font_bold modal_dark w-100  mt-4' type="submit" >Next</Button>
+              <Button
+                disabled={!formik.isValid || !formik.dirty}
+                className="m_0 text_white font_bold modal_dark w-100  mt-4"
+                type="submit"
+              >
+                Next
+              </Button>
             </form>
           </div>
-
         </Modal.Body>
       </Modal>
 
-
-
-      <Modal className='abc' show={companymodal} onHide={() => setCompanymodal(false)} aria-labelledby="contained-modal-title-vcenter"
-        centered>
-        <Modal.Body className='bg_primary radius_10'>
-          <div className='onboard_modal_sginup '>
-            <ArrowLeft className='text_white  mt-2 mb-2 ' style={{cursor: 'pointer'}}/>
-             
+      <Modal
+        className="abc"
+        show={companymodal}
+        onHide={() => setCompanymodal(false)}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Body className="bg_primary radius_10">
+          <div className="onboard_modal_sginup ">
+            <ArrowLeft
+              className="text_white  mt-2 mb-2 "
+              style={{ cursor: "pointer" }}
+            />
 
             <form onSubmit={formik2.handleSubmit}>
-
-              <p className='font_bold text_white mb-2'>Email</p>
-              <input id="email"
+              <p className="font_bold text_white mb-2">Email</p>
+              <input
+                id="email"
                 name="email"
                 type="text"
-                placeholder='Enter your email'
+                placeholder="Enter your email"
                 onChange={formik2.handleChange}
                 value={formik2.values.email}
-                className='input_felid' />
+                className="input_felid"
+              />
 
-              <p className='font_bold text_white mb-2 mt-2'>Phone number</p>
-              <input className='input_felid'
+              <p className="font_bold text_white mb-2 mt-2">Phone number</p>
+              <input
+                className="input_felid"
                 id="phone"
                 name="phone"
                 type="text"
-                placeholder='Enter your phone'
+                placeholder="Enter your phone"
                 onChange={formik2.handleChange}
-                value={formik2.values.phone} />
+                value={formik2.values.phone}
+              />
 
-
-
-              <p className='font_bold text_white mb-2 mt-2'>Linkdin url </p>
-              <input id="linkdin"
+              <p className="font_bold text_white mb-2 mt-2">Linkdin url </p>
+              <input
+                id="linkdin"
                 name="linkdin"
                 type="text"
-                placeholder='Enter your linkdin url'
+                placeholder="Enter your linkdin url"
                 onChange={formik2.handleChange}
-                value={formik2.values.linkdin} className='input_felid' />
+                value={formik2.values.linkdin}
+                className="input_felid"
+              />
 
-              <p className='font_bold text_white mb-2 mt-2'>Twitter url </p>
-              <input id="twitter"
+              <p className="font_bold text_white mb-2 mt-2">Twitter url </p>
+              <input
+                id="twitter"
                 name="twitter"
                 type="text"
-                placeholder='Enter your twitter url'
+                placeholder="Enter your twitter url"
                 onChange={formik2.handleChange}
-                value={formik2.values.twitter} className='input_felid' />
+                value={formik2.values.twitter}
+                className="input_felid"
+              />
 
-              <p className='font_bold text_white mb-2 mt-2'>Facebook url </p>
-              <input id="facebook"
+              <p className="font_bold text_white mb-2 mt-2">Facebook url </p>
+              <input
+                id="facebook"
                 name="facebook"
                 type="text"
-                placeholder='Enter your facebook url'
-
+                placeholder="Enter your facebook url"
                 onChange={formik2.handleChange}
-                value={formik2.values.facebook} className='input_felid' />
+                value={formik2.values.facebook}
+                className="input_felid"
+              />
 
-
-
-              <p className='font_bold text_white mb-2 mt-2'>Where is the company located</p>
-              <input id="located"
+              <p className="font_bold text_white mb-2 mt-2">
+                Where is the company located
+              </p>
+              <input
+                id="located"
                 name="located"
                 type="text"
-                placeholder='Enter you location'
-
+                placeholder="Enter you location"
                 onChange={formik2.handleChange}
-                value={formik2.values.located} className='input_felid' />
+                value={formik2.values.located}
+                className="input_felid"
+              />
 
-
-              <Button disabled={!formik2.isValid} className='m_0 text_white font_bold modal_dark w-100  mt-4 ' type="submit" >Next</Button>
+              <Button
+                disabled={!formik2.isValid}
+                className="m_0 text_white font_bold modal_dark w-100  mt-4 "
+                type="submit"
+              >
+                Next
+              </Button>
             </form>
           </div>
-
         </Modal.Body>
       </Modal>
 
-
-
-      <Modal show={questionmodal} onHide={() => setQuestionmodal(false)} aria-labelledby="contained-modal-title-vcenter"
-        centered>
-        <Modal.Body className='bg_primary radius_10'>
-          <div className='onboard_modal_sginup '>
+      <Modal
+        show={questionmodal}
+        onHide={() => setQuestionmodal(false)}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Body className="bg_primary radius_10">
+          <div className="onboard_modal_sginup ">
             <form onSubmit={formik3.handleSubmit}>
-              <p className='font_bold text_white mb-2 mt-2'>Why do you want to start a project in Switzerland?
+              <p className="font_bold text_white mb-2 mt-2">
+                Why do you want to start a project in Switzerland?
               </p>
-              <textarea id="why"
+              <textarea
+                id="why"
                 name="why"
                 type="text"
-                placeholder='Tell Somthing'
+                placeholder="Tell Somthing"
                 onChange={formik3.handleChange}
-                value={formik3.values.why} className='input_felid' style={{ height: 97 }} />
+                value={formik3.values.why}
+                className="input_felid"
+                style={{ height: 97 }}
+              />
 
-              <Button disabled={!formik3.isValid} className='m_0 text_white font_regular modal_dark w-100  mt-4' type="submit" >Submit</Button>
+              <Button
+                disabled={!formik3.isValid}
+                className="m_0 text_white font_regular modal_dark w-100  mt-4"
+                type="submit"
+              >
+                Submit
+              </Button>
             </form>
           </div>
-
         </Modal.Body>
       </Modal>
-
-
-
 
       <Modal
         aria-labelledby="contained-modal-title-vcenter"
         centered
-        show={showModal3} onHide={() => setShowModal3(false)}>
-        <Modal.Body className='onboard_modal' style={{ padding: 40 }}>
-          <p className='font_regular text_white fs_20 text-center'>Thanks for applying, we will be in touch! </p>
+        show={showModal3}
+        onHide={() => setShowModal3(false)}
+      >
+        <Modal.Body className="onboard_modal" style={{ padding: 40 }}>
+          <p className="font_regular text_white fs_20 text-center">
+            Thanks for applying, we will be in touch!{" "}
+          </p>
         </Modal.Body>
       </Modal>
-
     </>
-
-  )
+  );
 }
